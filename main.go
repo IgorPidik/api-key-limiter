86c117d546ed0b94c0d69c433d1afceba35b9ac7
Igor Pidik <IgorPidik@gmail.com>
2024-12-01 18:24:15 +0100

    clean up

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"api-key-limiter/proxy"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	url := "localhost:8000"
-	fmt.Printf("Starting Proxy server on %s\n", url)
+	url := "0.0.0.0:8000"
+	log.Printf("Starting Proxy server on %s\n", url)
 	proxy := proxy.NewProxy()
 	log.Fatal(http.ListenAndServe(url, proxy))
 }
