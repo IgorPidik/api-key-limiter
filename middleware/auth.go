9d6bc88805c372047466092c635f15bf51cd0249
Igor Pidik <IgorPidik@gmail.com>
2025-02-01 21:35:17 +0100

    clean up

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"api-key-limiter/handlers"
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -25,7 +24,6 @@ func NewAuthMiddleware(projectHandler *handlers.ProjectHandler) *AuthMiddleware 
 func (a *AuthMiddleware) Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		fmt.Printf("project header: %v\n", r.Header)
 		authHeader, parsingErr := ParseAuthHeader(r.Header.Get("Proxy-Authorization"))
 		if parsingErr != nil {
 			http.Error(w, "Invalid auth header", http.StatusBadRequest)
